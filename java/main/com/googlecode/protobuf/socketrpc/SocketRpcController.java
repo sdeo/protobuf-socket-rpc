e761b79c1314694c783e3810aa6e4b5116ab1445
shardul.deo <shardul.deo@b3a271fa-f289-11dd-8c4e-4bbb75a408b7>
2009-02-05 05:16:39 +0000

    Initial checking.

---
+++ java/main/com/googlecode/protobuf/socketrpc/SocketRpcController.java
@@ -0,0 +1,50 @@
+package com.googlecode.protobuf.socketrpc;
+
+import com.google.protobuf.RpcCallback;
+import com.google.protobuf.RpcController;
+
+public class SocketRpcController implements RpcController {
+  
+  boolean success = false;
+  String error = null;
+
+  @Override
+  public String errorText() {
+    return error;
+  }
+
+  @Override
+  public boolean failed() {
+    return success;
+  }
+  
+  @Override
+  public boolean isCanceled() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void notifyOnCancel(RpcCallback<Object> callback) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void reset() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void setFailed(String reason) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void startCancel() {
+    // TODO Auto-generated method stub
+    
+  }
+}
