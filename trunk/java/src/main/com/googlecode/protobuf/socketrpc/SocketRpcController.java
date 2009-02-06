c99c15cc57bf717d9ecdce586652791331b72299
shardul.deo <shardul.deo@b3a271fa-f289-11dd-8c4e-4bbb75a408b7>
2009-02-06 08:23:16 +0000

    Added lots of error handling. setFailed on the controller is propagated back to client side. Also now propagates if the callback is called at all or called with a null to the client side.

--- trunk/java/src/main/com/googlecode/protobuf/socketrpc/SocketRpcController.java
+++ trunk/java/src/main/com/googlecode/protobuf/socketrpc/SocketRpcController.java
@@ -3,10 +3,26 @@ package com.googlecode.protobuf.socketrpc;
 import com.google.protobuf.RpcCallback;
 import com.google.protobuf.RpcController;
 
+/**
+ * Simple controller.
+ * 
+ * @author Shardul Deo
+ */
 public class SocketRpcController implements RpcController {
   
   boolean success = false;
   String error = null;
+
+  @Override
+  public void reset() {
+    success = false;
+    error = null;
+  }
+
+  @Override
+  public boolean failed() {
+    return !success;
+  }
 
   @Override
   public String errorText() {
@@ -14,37 +30,29 @@ public class SocketRpcController implements RpcController {
   }
 
   @Override
-  public boolean failed() {
-    return !success;
-  }
-  
-  @Override
-  public boolean isCanceled() {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  @Override
-  public void notifyOnCancel(RpcCallback<Object> callback) {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void reset() {
-    // TODO Auto-generated method stub
-    
+  public void startCancel() {
+    // Not yet supported
+    throw new UnsupportedOperationException(
+        "Cannot cancel request in Socket RPC");
   }
 
   @Override
   public void setFailed(String reason) {
-    // TODO Auto-generated method stub
-    
+    success = false;
+    error = reason;
   }
 
   @Override
-  public void startCancel() {
-    // TODO Auto-generated method stub
-    
+  public boolean isCanceled() {
+    // Not yet supported
+    throw new UnsupportedOperationException(
+        "Cannot cancel request in Socket RPC");
+  }
+
+  @Override
+  public void notifyOnCancel(RpcCallback<Object> callback) {
+    // Not yet supported
+    throw new UnsupportedOperationException(
+        "Cannot cancel request in Socket RPC");
   }
 }
