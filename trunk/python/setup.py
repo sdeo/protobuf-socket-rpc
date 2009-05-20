57462257d900e12c5c9ba700b062e809ca318996
martin.norbury <martin.norbury@b3a271fa-f289-11dd-8c4e-4bbb75a408b7>
2009-05-20 15:48:15 +0000

    Changed version number to be in sync with Java release

--- trunk/python/setup.py
+++ trunk/python/setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 setup(
       name = "protobuf_socket_rpc",
-      version = "0.1",
+      version = "1.2",
       packages = find_packages('src/main'),
       package_dir = {'':'src/main'},      
       )
